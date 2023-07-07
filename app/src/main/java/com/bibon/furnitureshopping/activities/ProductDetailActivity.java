be85f644aef45ace12b1b70af020eb00cd5ffa4f
K <thekhoi.k@gmail.com>
2023-07-07 21:41:57 +0700

    edit detail

--- app/src/main/java/com/bibon/furnitureshopping/activities/ProductDetailActivity.java
+++ app/src/main/java/com/bibon/furnitureshopping/activities/ProductDetailActivity.java
@@ -1,12 +1,14 @@
 package com.bibon.furnitureshopping.activities;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
 import com.bibon.furnitureshopping.R;
 import com.bibon.furnitureshopping.models.Product;
@@ -26,7 +28,21 @@ public class ProductDetailActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_product_detail);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        // Sets the Toolbar to act as the ActionBar for this Activity window.
+        // Make sure the toolbar exists in the activity and is not null
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayShowTitleEnabled(false);
+        toolbar.setTitle("");
+        toolbar.setSubtitle("");
+        ImageView img_back = (ImageView) findViewById(R.id.img_back);
 
+        img_back.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                onBackPressed();
+            }
+        });
         tv_product_name_detail = (TextView) findViewById(R.id.tv_product_name_detail);
         tv_price = (TextView) findViewById(R.id.tv_price);
         tv_quantity = (TextView) findViewById(R.id.tv_quantity);
