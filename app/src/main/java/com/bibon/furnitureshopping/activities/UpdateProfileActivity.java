8d872592727985207c56a6e15867b5925809eb79
nobi-onway <doangiabao276@gmail.com>
2023-07-14 17:10:14 +0700

    update

--- app/src/main/java/com/bibon/furnitureshopping/activities/UpdateProfileActivity.java
+++ app/src/main/java/com/bibon/furnitureshopping/activities/UpdateProfileActivity.java
@@ -9,7 +9,7 @@ import android.graphics.drawable.Drawable;
 import android.net.Uri;
 import android.os.Bundle;
 import android.provider.MediaStore;
-import android.support.annotation.NonNull;
+import androidx.annotation.NonNull;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
