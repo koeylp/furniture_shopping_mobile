a8b06aaf6808159df71a9aaf6ec21bc667ae44d3
truonggiang0405 <vutruonggiang452002@gmail.com>
2023-07-18 18:38:37 +0700

    ADD NOTI

---
+++ app/src/main/java/com/bibon/furnitureshopping/NotificationActivity.java
@@ -0,0 +1,14 @@
+package com.bibon.furnitureshopping;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class NotificationActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_notification);
+    }
+}
